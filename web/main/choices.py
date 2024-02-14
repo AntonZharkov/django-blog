65c7e7885e37fbc85a96506841257f4a4090f0a1
AntonZharkov <19antoni88@gmail.com>
2024-02-14 22:13:54 +0300

    added Provider choices

--- web/main/choices.py
+++ web/main/choices.py
@@ -1,4 +1,4 @@
-from django.db.models import IntegerChoices
+from django.db.models import IntegerChoices, TextChoices
 from django.utils.translation import gettext_lazy as _
 
 
@@ -6,3 +6,8 @@ class UserGender(IntegerChoices):
     UNKNOWN = (0, _("Unknown"))
     MAN = (1, _("Man"))
     FEMALE = (2, _("Female"))
+
+
+class SocialProvider(TextChoices):
+    GITHUB = "github"
+    GOOGLE = "google"
